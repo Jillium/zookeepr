4904f38cbf3e46221c5483473b8964704a11b04c
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-15 11:48:44 -0700

    userDataCreation

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
+const { dirname } = require('path/posix');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parson incoming JSON data
+app.use(express.json());
 
 
 function filterByQuery(query, animalsArray) {
@@ -49,6 +56,35 @@ function findById(id, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+      const animal = body;
+      animalsArray.push(animal);
+      fs.writeFileSync(
+          path.join(__dirname, './data/animals.json'),
+          JSON.stringify({ animals: animalsArray}, null, 2)
+      );
+      // our function's main code will go here!
+
+      // return finished code to post route for response 
+      return animal;
+  }
+
+  function validateAnimal(animal) {
+      if (!animal.name || typeof animal.name !== 'string') {
+          return false;
+      } 
+      if (!animal.species || typeof animal.species !== 'string') {
+          return false;
+      }
+      if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+      }
+      if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+          return false;
+      }
+      return true;
+  }
+
 // get method that requires two arguments
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -68,6 +104,24 @@ app.get('/api/animals/:id', (req, res) => {
       else {
           res.send(404);
       }
+  });
+
+  app.post('/api/animals', (req, res) => {
+      // set id based on what the next index of the array will be
+      req.body.id = animals.length.toString();
+
+      // if any data in req.body is incorrect, send 400 error back
+      if (!validateAnimal(req.body)) {
+          res.status(400).send('The animal is not properly formatted.');
+      } else {
+           // add animal to json file and animals array in this function
+      const animal = createNewAnimal(req.body, animals);
+      
+      
+      res.json(animal);
+      }
+
+     
   });
 
 
